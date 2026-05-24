31582c4de848ae92fcb5e72b63109d06f4a5a38a
moreennsanyu-alt <moreenn.sanyu@gmail.com>
2026-05-24 08:03:32 +0300

    Last Sync: 2026-05-24 08:03 (Mobile)

---
+++ Src/Desktop.Tests/ClinicManager.Win.Tests/GlobalUsings.cs
@@ -0,0 +1,6 @@
+global using ClinicManager.Domain.Common;
+global using ClinicManager.Domain.Entities;
+global using ClinicManager.Domain.Enums;
+global using ClinicManager.Domain.Events;
+global using ClinicManager.Domain.Exceptions;
+global using ClinicManager.Domain.ValueObjects;
