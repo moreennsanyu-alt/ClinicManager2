49241639b0e1dde561eddb3ee1dbd2364472d782
moreennsanyu-alt <252101504+moreennsanyu-alt@users.noreply.github.com>
2026-06-06 06:56:30 +0000

    chore(deps): update paket dependencies

---
+++ ClinicManager/ClinicManager.E2E.Tests/Tests.cs
@@ -0,0 +1,42 @@
+﻿using DevExpress.EasyTest.Framework;
+using Xunit;
+
+[assembly: CollectionBehavior(DisableTestParallelization = true)]
+
+// To run functional tests for ASP.NET Web Forms and ASP.NET Core Blazor XAF Applications,
+// install browser drivers: https://www.selenium.dev/documentation/getting_started/installing_browser_drivers/.
+//
+// -For Google Chrome: download "chromedriver.exe" from https://chromedriver.chromium.org/downloads.
+// -For Microsoft Edge: download "msedgedriver.exe" from https://developer.microsoft.com/en-us/microsoft-edge/tools/webdriver/.
+//
+// Selenium requires a path to the downloaded driver. Add a folder with the driver to the system's PATH variable.
+//
+// Refer to the following article for more information: https://docs.devexpress.com/eXpressAppFramework/403852/
+
+namespace ClinicManager.Module.E2E.Tests;
+
+public class ClinicManagerTests : IDisposable {
+    const string WinAppName = "ClinicManagerWin";
+    const string AppDBName = "ClinicManager";
+    EasyTestFixtureContext FixtureContext { get; } = new EasyTestFixtureContext();
+
+    public ClinicManagerTests() {
+        FixtureContext.RegisterApplications(
+            new WinApplicationOptions(WinAppName, string.Format(@"{0}\..\..\..\..\ClinicManager.Win\bin\EasyTest\net9.0-windows\ClinicManager.Win.exe", Environment.CurrentDirectory))
+        );
+        FixtureContext.RegisterDatabases(new DatabaseOptions(AppDBName, "ClinicManagerEasyTest", server: @"Sqlite"));
+    }
+    public void Dispose() {
+        FixtureContext.CloseRunningApplications();
+    }
+    [Theory]
+    [InlineData(WinAppName)]
+    public void TestWinApp(string applicationName) {
+        FixtureContext.DropDB(AppDBName);
+        var appContext = FixtureContext.CreateApplicationContext(applicationName);
+        appContext.RunApplication();
+        Assert.True(appContext.Navigate("My Details"));
+        Assert.True(appContext.Navigate("Role"));
+        Assert.True(appContext.Navigate("Users"));
+    }
+}
---
+++ ClinicManager/ClinicManager.MiddleTier/Startup.cs
@@ -0,0 +1,105 @@
+﻿using DevExpress.ExpressApp.Security;
+using DevExpress.Persistent.Base;
+using DevExpress.ExpressApp.Xpo;
+using DevExpress.Persistent.BaseImpl.PermissionPolicy;
+using System.Text;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+using DevExpress.ExpressApp;
+using DevExpress.ExpressApp.ApplicationBuilder;
+
+namespace ClinicManager.WebApi;
+
+public class Startup {
+    public Startup(IConfiguration configuration) {
+        Configuration = configuration;
+    }
+
+    public IConfiguration Configuration { get; }
+
+    // This method gets called by the runtime. Use this method to add services to the container.
+    // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+    public void ConfigureServices(IServiceCollection services) {
+
+        services.AddXafMiddleTier(Configuration, builder => {
+            builder.ConfigureDataServer(options => {
+                options.UseConnectionString(Configuration.GetConnectionString("ConnectionString"));
+                options.UseDataStorePool(true);
+            });
+
+            builder.Modules
+                .Add<ClinicManager.Module.ClinicManagerModule>();
+
+
+            builder.Security
+                .UseIntegratedMode(options => {
+                    options.Lockout.Enabled = true;
+
+                    options.RoleType = typeof(PermissionPolicyRole);
+                    // ApplicationUser descends from PermissionPolicyUser and supports the OAuth authentication. For more information, refer to the following topic: https://docs.devexpress.com/eXpressAppFramework/402197
+                    // If your application uses PermissionPolicyUser or a custom user type, set the UserType property as follows:
+                    options.UserType = typeof(ClinicManager.Module.BusinessObjects.ApplicationUser);
+                    // ApplicationUserLoginInfo is only necessary for applications that use the ApplicationUser user type.
+                    // If you use PermissionPolicyUser or a custom user type, comment out the following line:
+                    options.UserLoginInfoType = typeof(ClinicManager.Module.BusinessObjects.ApplicationUserLoginInfo);
+                    options.UseXpoPermissionsCaching();
+                    options.Events.OnSecurityStrategyCreated += securityStrategy => {
+                        //((SecurityStrategy)securityStrategy).AnonymousAllowedTypes.Add(typeof(DevExpress.Persistent.BaseImpl.ModelDifference));
+                        //((SecurityStrategy)securityStrategy).AnonymousAllowedTypes.Add(typeof(DevExpress.Persistent.BaseImpl.ModelDifferenceAspect));
+                        ((SecurityStrategy)securityStrategy).PermissionsReloadMode = PermissionsReloadMode.CacheOnFirstAccess;
+                    };
+                });
+
+            builder.AddBuildStep(application => {
+                application.ApplicationName = "SetupApplication.ClinicManager";
+                application.CheckCompatibilityType = DevExpress.ExpressApp.CheckCompatibilityType.DatabaseSchema;
+                //application.CreateCustomModelDifferenceStore += (s, e) => {
+                    //    e.Store = new ModelDifferenceDbStore((XafApplication)sender!, typeof(ModelDifference), true, "Win");
+                    //    e.Handled = true;
+                //};
+#if !RELEASE
+                if(application.CheckCompatibilityType == CheckCompatibilityType.DatabaseSchema) {
+                    application.DatabaseUpdateMode = DatabaseUpdateMode.UpdateDatabaseAlways;
+                    application.DatabaseVersionMismatch += (s, e) => {
+                        e.Updater.Update();
+                        e.Handled = true;
+                    };
+                }
+#endif
+            });
+        });
+
+        services.AddAuthentication()
+
+        services.AddAuthorization(options => {
+            options.DefaultPolicy = new AuthorizationPolicyBuilder(
+                .RequireAuthenticatedUser()
+                .RequireXafAuthentication()
+                .Build();
+        });
+    }
+
+    // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env, IHostApplicationLifetime hostApplicationLifetime) {
+        if(env.IsDevelopment()) {
+            app.UseDeveloperExceptionPage();
+        }
+        else {
+            app.UseExceptionHandler("/Error");
+            // The default HSTS value is 30 days. To change this for production scenarios, see: https://aka.ms/aspnetcore-hsts.
+            app.UseHsts();
+        }
+        app.UseHttpsRedirection();
+        app.UseRequestLocalization();
+        app.UseStaticFiles();
+        app.UseRouting();
+        app.UseAuthentication();
+        app.UseAuthorization();
+        app.UseXafMiddleTier();
+        app.UseEndpoints(endpoints => {
+            endpoints.MapControllers();
+        });
+    }
+}
